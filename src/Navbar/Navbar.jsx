e24d58a3116d777d3ade8bb6770a4d755af695a2
Vkeeycode <chinomsovinfaith@gmail.com>
2023-12-08 05:07:25 +0100

    PofileCard project

---
+++ src/Navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./navbar.css";
+const Navbar = () => {
+  return (
+    <nav>
+      <ul className="links">
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
